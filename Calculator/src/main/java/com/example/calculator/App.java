e87b0aeae01c33c8f63decf776d69cb62e829206
Hallie <hine113@gmail.com>
2021-09-13 09:31:43 -0500

    Recreate Calculator App

---
+++ Calculator/src/main/java/com/example/calculator/App.java
@@ -0,0 +1,22 @@
+package com.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("calculator-view.fxml"));
+
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
